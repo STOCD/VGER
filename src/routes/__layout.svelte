d8ff3e5efa547ea23d36d618e7489af9fe297920
Lewis <lewis@ombulabs.com>
2022-01-21 09:35:23 -0500

    add chevrons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,15 @@
 <script>
   import '../app.css';
   import Banner from '$lib/banner/Banner.svelte';
+  import Chevron from '$lib/assets/starfleet-chevron-science-grey.png';
 </script>
 
 <Banner />
 <div>
-  <h1>Visual Glossary Educational Repository</h1>
+  <h1>
+    <span>Visual Glossary Educational Repository</span>
+    <img src={Chevron} alt='Starfleet science chevron' />
+  </h1>
   <slot />
 </div>
 
@@ -17,10 +21,21 @@
     height: 100%;
   }
   h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     text-align: center;
     text-transform: uppercase;
     font-size: 1.5rem;
     color: white;
     margin: 0 0 var(--gutter);
+    position: relative;
+    padding-right: 0.5rem;
+  }
+
+  img {
+    height: 2rem;
+    width: 1.25rem;
+    margin-left: 0.5rem;
   }
 </style>
