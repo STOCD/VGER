c1954a9e62688ba59dcff8c93cfa8cabcbc1ecf6
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2022-06-02 08:40:54 +0200

    Delete src directory

--- src/lib/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-export const activeTab = writable('Visual Glossary Educational Repository');
-
-export const activeCard = writable('');
-
-export const srcValue = writable('');
-
-export const filtered = writable([]);
