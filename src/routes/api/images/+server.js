e477eb531b8802cd4a798181718bf60678de4836
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2024-02-23 19:55:47 +0100

    undo debug and fix the fix

--- src/routes/api/images/+server.js
+++ src/routes/api/images/+server.js
@@ -11,13 +11,13 @@ export async function PUT({url}) {
     const param_image = url.searchParams.get('image');
     const param_type = url.searchParams.get('type');
     const param_force = url.searchParams.get('force');
-    force_upload = param_force !== '1' ? false : true;
+    let force_upload = param_force !== '1' ? false : true;
     if (param_image === null || param_image === '' || param_type === null || param_type === '') {
         return new Response('Invalid parameters!', {status: 400})
     }
     const image_data = await fetch_image(param_image);
     const put_status = await store_image(param_type, image_data, param_image, force_upload);
-    return new Response(`${wiki_images}${param_image}`, {body: put_status})
+    return new Response(`${wiki_images}${param_image}`, {status: put_status})
 }
 
 export async function GET({url}) {
@@ -56,7 +56,7 @@ async function store_image(type, image_data, name, force = false) {
             )
         });
         const stat = await response.json()
-        return stat
+        return stat.status
     }
     return 200
 }
